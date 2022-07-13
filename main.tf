8018162774836b3d66a7350d07762e296a85702b
genie9090 <eugenewhitie@gmail.com>
2022-07-13 15:56:27 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "vm" {
+resource "aws_instance" "vm1" {
   ami           = "DUMMY_VALUE_AMI_ID"
   subnet_id     = "DUMMY_VALUE_SUBNET_ID"
   instance_type = "t3.micro"
